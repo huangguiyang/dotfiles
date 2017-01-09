bed598b1fee4d763df9b019b6b4980335784d69a
mario <mohu3g@163.com>
2017-01-09 09:44:29 +0800

    update helm-find-files-map for <tab>

--- configs/init-helm.el
+++ configs/init-helm.el
@@ -7,6 +7,7 @@
 (global-set-key (kbd "C-x r b") #'helm-filtered-bookmarks)
 (global-set-key (kbd "C-x C-f") #'helm-find-files)
 (global-set-key (kbd "C-x C-b") #'helm-buffers-list)
+(define-key helm-find-files-map (kbd "<tab>") #'helm-execute-persistent-action)
 
 ;; helm-gtags
 (package-install 'helm-gtags)
