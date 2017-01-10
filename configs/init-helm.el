ce645422a9a0a1427ac9eb3a40a842de610fe5ec
mario <mohu3g@163.com>
2017-01-10 15:52:30 +0800

    set helm-grep-ignore-fiels

--- configs/init-helm.el
+++ configs/init-helm.el
@@ -18,4 +18,7 @@
 (global-set-key (kbd "C-x b") 'helm-mini)
 (global-set-key (kbd "C-s") 'helm-occur)
 
+(dolist (f '("GPATH" "GTAGS" "GRTAGS" ".clang-format" ".depend"))
+  (add-to-list 'helm-grep-ignored-files f))
+
 (provide 'init-helm)
