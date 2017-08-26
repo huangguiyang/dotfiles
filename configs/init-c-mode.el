67045758331f24cb3ba1092b48a51bce3e1058ce
hgy <guiyang.huang@gmail.com>
2017-08-26 12:59:22 +0800

    update comment-start

--- configs/init-c-mode.el
+++ configs/init-c-mode.el
@@ -1,7 +1,7 @@
 
 (setq c-basic-offset 4)
 (setq c-label-minimum-indentation 0)
-(add-hook 'c-mode-hook (lambda () (setq comment-start "//"
+(add-hook 'c-mode-hook (lambda () (setq comment-start "// "
                                         comment-end "")))
 
 (package-install 'company-c-headers)
