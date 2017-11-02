12393b2bb81447cb6ca4905a02705f642d596d8f
mario <mohu3g@163.com>
2017-11-02 08:52:05 +0800

    update theme

--- configs/init-theme.el
+++ configs/init-theme.el
@@ -1,13 +1,13 @@
 
-;; (package-install 'solarized-theme)
-;; (load-theme 'solarized-dark t)
+(package-install 'solarized-theme)
+(load-theme 'solarized-dark t)
 
 ;; (load-theme 'adwaita)
 
 ;; (package-install 'github-modern-theme)
 ;; (load-theme 'github-modern t)
 
-(package-install 'github-theme)
-(load-theme 'github t)
+;; (package-install 'github-theme)
+;; (load-theme 'github t)
 
 (provide 'init-theme)
