df3dd5190ee047b5340509285783142babf30aa7
mario <guiyang.huang@gmail.com>
2018-11-28 16:24:46 +0800

    update c mode comment sep

--- init-c-mode.el
+++ init-c-mode.el
@@ -1,8 +1,8 @@
 
 (setq c-basic-offset 4)
 (setq c-label-minimum-indentation 0)
-(add-hook 'c-mode-hook (lambda () (setq comment-start "// "
-                                        comment-end "")))
+(add-hook 'c-mode-hook (lambda () (setq comment-start "/* "
+                                        comment-end " */")))
 
 (package-install 'company-c-headers)
 (add-hook 'c-mode-hook
